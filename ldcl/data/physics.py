28bc5cb67462f027eeb6494e4f5d1121bed893ed
Ali Cy <califynic@gmail.com>
2022-12-06 17:15:29 -0500

    Adding orbits to config file and syncing it with run scripts
    Major changes overall. I added orbits to the
    config file and then I changed the run scripts
    so they'd work with it.

--- ldcl/data/physics.py
+++ ldcl/data/physics.py
@@ -12,9 +12,12 @@ import os
 import shutil
 
 import matplotlib.pyplot as plt
+import torch
 
-rng = np.random.default_rng()
+#rng = np.random.default_rng()
+rng = np.random.default_rng(9)  # manually seed random number generator
 verbose = True
+MAX_ITERATIONS = 100
 
 def read_config(f):
     """
@@ -31,19 +34,36 @@ def read_config(f):
 
     def convert_keys(d):
         for key, value in d.items():
-            if key in ["None", "True", "False"] or "[" in key:
+            if value in ["None", "True", "False"]:
                 d[key] = eval(d[key])
+            elif isinstance(value, str) and "," in value:
+                d[key] = value.split(",")
+                for idx, v in enumerate(d[key]):
+                    d[key][idx] = float(v)
             elif isinstance(value, dict):
                 convert_keys(value)
+
+    convert_keys(x)
 
     return DefaultMunch.fromDict(x, object())
 
 def sample_distribution(dist, num):
     if dist.type == "uniform":
+        ret = []
+
+        if dist.dims == 1 and type(dist.max) == int:
+            dist.max = [dist.max]
+            dist.min = [dist.min]
+
+        for dim in range(dist.dims):
+            ret.append(rng.uniform(dist.min[dim], dist.max[dim], size=num))
+        
         if dist.dims == 1:
-            return rng.uniform(dist.min, dist.max, size=num)
+            dist.max = dist.max[0] # Undo modifications
+            dist.min = dist.min[0]
+            return ret[0]
         else:
-            raise NotImplementedError # implement multi-d
+            return np.stack(ret, axis=-1)
     elif dist.type == "uniform_with_intervals":
         # Note: this is uniform across the union of all intervals,
         # i.e. if some intervals are longer than other intervals,
@@ -83,17 +103,17 @@ def pendulum_num_gen(config):
     """
     settings = config.pendulum_settings
 
-    t = np.reshape(sample_distribution(settings.t_distr, settings.num_ts * settings.num_energies), (settings.num_energies, settings.num_ts))
+    t = np.reshape(sample_distribution(settings.t_distr, settings.num_ts * settings.num_trajs), (settings.num_trajs, settings.num_ts))
     if config.modality == "image":
         t = np.stack((t, t + config.pendulum_imagen_settings.diff_time), axis=-1) # time steps
-    k2 = sample_distribution(settings.energy_distr, settings.num_energies)[:, np.newaxis, np.newaxis]
+    k2 = sample_distribution(settings.traj_distr, settings.num_trajs)[:, np.newaxis, np.newaxis]
 
     sn, cn, dn, _ = ellipj(t, k2) # fix this more
     q = 2 * np.arcsin(np.sqrt(k2) * sn)
     p = 2 * np.sqrt(k2) * cn * dn / np.sqrt(1 - k2 * sn ** 2) # anglular momentum
     data = np.stack((q, p), axis=-1)
 
-    if settings.pq_resample_condition == None:
+    if settings.pq_resample_condition is not None:
         raise NotImplementedError # Resampling is going to be hard!4
 
     if settings.shuffle:
@@ -215,8 +235,117 @@ def pendulum_img_gen(config, bundle):
     
     return {
         "k2": np.broadcast_arrays(bundle["k2"], q)[0],
-        "imgs": pxls,
-        "q": q
+        "q": q,
+        "data": pxls
+    }
+
+def eccentric_anomaly_from_mean(e, M, tol=1e-13):
+    """Convert mean anomaly to eccentric anomaly.
+    Implemented from [A Practical Method for Solving the Kepler Equation][1]
+    by Marc A. Murison from the U.S. Naval Observatory
+    [1]: http://murison.alpheratz.net/dynamics/twobody/KeplerIterations_summary.pdf
+    """
+    Mnorm = np.fmod(M, 2 * np.pi)
+    E0 = np.fmod(M + (-1 / 2 * e ** 3 + e + (e ** 2 + 3 / 2 * np.cos(M) * e ** 3) * np.cos(M)) * np.sin(M), 2 * np.pi)
+    dE = tol + 1
+    count = 0
+    while np.any(dE > tol):
+        t1 = np.cos(E0)
+        t2 = -1 + e * t1
+        t3 = np.sin(E0)
+        t4 = e * t3
+        t5 = -E0 + t4 + Mnorm
+        t6 = t5 / (1 / 2 * t5 * t4 / t2 + t2)
+        E = E0 - t5 / ((1 / 2 * t3 - 1 / 6 * t1 * t6) * e * t6 + t2)
+        dE = np.abs(E - E0)
+        E0 = np.fmod(E, 2 * np.pi)
+        count += 1
+        if count == MAX_ITERATIONS:
+            print('Current dE: ', dE[dE > tol])
+            print('eccentricity: ', np.repeat(e, dE.shape[-1], axis=-1)[dE > tol])
+            raise RuntimeError(f'Did not converge after {MAX_ITERATIONS} iterations with tolerance {tol}.')
+    return E
+
+def orbits_num_gen(config):
+    #batch_size, traj_samples=100, noise=0., shuffle=True, check=False, H=None, L=None, phi0=None):
+    settings = config.orbit_settings
+
+    mu = settings.mu  # standard gravitational parameter, i.e. G*M
+
+    E = None
+    while E is None:
+        #t = rng.uniform(0, 10. * traj_samples, size=(batch_size, traj_samples))
+        t = sample_distribution(settings.t_distr, settings.num_trajs * settings.num_ts).reshape((settings.num_trajs, settings.num_ts))
+
+        all_conserved = sample_distribution(settings.traj_distr, settings.num_trajs)
+        H, L, phi0 = tuple([all_conserved[..., i][..., np.newaxis] for i in range(3)])
+
+        #H = -mu / 2 * (0.5 + 0.5 * rng.uniform(size=(batch_size, 1))) if H is None else H * np.ones((batch_size, 1)) # samples uniformly from -1/4 to -1/2
+        #L = rng.uniform(size=(batch_size, 1)) if L is None else L * np.ones((batch_size, 1))
+
+        H = -mu / 2 * (0.5 + 0.5 * H)
+
+        a = -mu / (2 * H)  # semi-major axis
+        e = np.sqrt(1 - L ** 2 / (mu * a))
+
+        #print('eshape =', e.shape)
+        #print('tshape =', t.shape)
+
+        #phi0 = 2 * np.pi * rng.uniform(size=(batch_size, 1)) if phi0 is None else phi0 * np.ones((batch_size, 1))
+
+        # https://downloads.rene-schwarz.com/download/M001-Keplerian_Orbit_Elements_to_Cartesian_State_Vector/.pdf
+        T = 2 * np.pi * np.sqrt(a ** 3 / mu)  # period
+        M = np.fmod(2 * np.pi * t / T, 2 * np.pi)  # mean anomaly
+        
+        try:
+            E = eccentric_anomaly_from_mean(e, M)  # eccentric anomaly
+        except:
+            print("data generation failed.")
+            pass
+
+    phi = 2 * np.arctan2(np.sqrt(1 + e) * np.sin(E / 2), np.sqrt(1 - e) * np.cos(E / 2))  # true anomaly/angle
+    r = (a * (1 - e ** 2)) / (1 + e * np.cos(phi))  # radius
+    pos = np.stack((r * np.cos(phi + phi0), r * np.sin(phi + phi0)), axis=-1)  # position rotated by phi0
+
+    vel = np.expand_dims(np.sqrt(mu * a) / r, axis=-1) * np.stack((-np.sin(E), np.sqrt(1 - e ** 2) * np.cos(E)),
+                                                                  axis=-1)  # velocity
+    c, s = np.cos(phi0), np.sin(phi0)
+    R = np.stack((c, -s, s, c), axis=-1).reshape(-1, 1, 2, 2)
+    vel = np.squeeze(R @ np.expand_dims(vel, axis=-1), axis=-1)  # rotated by phi0
+
+    data = np.concatenate((pos, vel), axis=-1)
+    #print('data shape =', data.shape)
+
+    if settings.check:
+        assert np.allclose(M, E - e * np.sin(E))
+
+        p = np.sqrt(mu * (2 / r - 1 / a))  # speed/specific momentum
+        diffp = p - np.linalg.norm(vel, axis=-1)
+        # print(e[np.any(np.isnan(diffp),axis=-1)])
+        assert np.allclose(diffp, np.zeros_like(diffp))
+
+        L = np.sqrt(mu * a * (1 - e ** 2))  # specific angular momentum
+        diffL = L - np.cross(pos, vel)
+        assert np.allclose(diffL, np.zeros_like(diffL))
+
+        H = -mu / (2 * a)  # specific energy
+        diffH = H - (0.5 * np.linalg.norm(vel, axis=-1) ** 2 - mu / np.linalg.norm(pos, axis=-1))
+        assert np.allclose(diffH, np.zeros_like(diffH))
+
+    if settings.shuffle:
+        for x in data:
+            rng.shuffle(x, axis=0)
+
+    if settings.noise > 0:
+        data += settings.noise * rng.standard_normal(size=data.shape)
+
+    return {
+        "e": e,
+        "a": a,
+        "phi0": phi0,
+        "H": H,
+        "L": L,
+        "data": data
     }
 
 """
@@ -224,7 +353,7 @@ def pendulum_img_gen(config, bundle):
 imgs = np.swapaxes(imgs, 4, 2)
 imgs = imgs * 255
 imgs = imgs.astype(np.uint8)
-for j in range(0, config.pendulum_settings.num_energies):
+for j in range(0, config.pendulum_settings.num_trajs):
     for i in range(0, config.pendulum_settings.num_ts):
         img = Image.fromarray(imgs[i,j,:,:,:], 'RGB')
         img.show()
@@ -232,52 +361,109 @@ for j in range(0, config.pendulum_settings.num_energies):
         input("continue...")
 """
 
-def get_dataset(config):
+class ConservationDataset(torch.utils.data.Dataset):
+    def __init__(self, bundle):
+        self.size = bundle["data"].shape[0]
+        self.data = bundle["data"] # assuming this is where the actual data is
+        del bundle["data"]
+        self.bundle = bundle
+
+    def __getitem__(self, idx):
+        if idx < self.size:
+            x_data = self.data[idx]
+            random_x_rows = np.random.randint(0,x_data.shape[0],2) # generate two different views
+
+            x_output = x_data[random_x_rows]
+            x_view1 = x_output[0]
+            x_view2 = x_output[1]
+
+            return [x_view1,x_view2, {k: v[idx] for k, v in self.bundle.items()}]
+        else:
+            raise ValueError
+
+    def __len__(self):
+        return self.size
+        
+
+def get_dataset(config, saved_dir):
     """
         General dataset generation.
 
         Eventually, we'll probably read the config file in the training loop and then just pass it in here as an object...
 
         :param config: Configuration file to receive parameters in, or already parsed configuration object.
-        :return: string with directory with the dataset
+        :return: Dataset object, and then name of folder inside saved_dir where data can be found
     """
     if isinstance(config, str):
         config = read_config(config)
 
     # If cache, check if exists
+    bundle = None
     if config.use_cached_data:
-        for other_file in glob.glob("../saved_datasets/*/config.json"):
-            input(other_file)
-            if read_config(other_file) == config:
+        for other_file in glob.glob(saved_dir + "/*/config.json"):
+            if json.dumps(read_config(other_file)) == json.dumps(config):
                 other_file = other_file[:other_file.rindex("/")]       
-                return other_file[other_file.rindex("/") + 1:]
+                folder_name = other_file[other_file.rindex("/") + 1:]
+                
+                bundle = {}
+                for npfile in glob.glob(other_file + "/*.npy"):
+                    name = npfile[:npfile.rindex(".")]
+                    name = name[name.rindex("/") + 1:]
+                    bundle[name] = np.load(npfile)
 
     # Generate data
-    if config.dynamics == "pendulum":
-        bundle = pendulum_num_gen(config)
-        if config.modality == "image":
-            bundle = pendulum_img_gen(config, bundle)
+    if bundle is None:
+        if config.dynamics == "pendulum":
+            bundle = pendulum_num_gen(config)
+            if config.modality == "image":
+                bundle = pendulum_img_gen(config, bundle)
+            elif config.modality == "numerical":
+                bundle = bundle
+            else:
+                raise ValueError("Config modality not specified")
+        elif config.dynamics == "orbits":
+            bundle = orbits_num_gen(config)
+            if config.modality == "image":
+                raise NotImplementedError
+            elif config.modality == "numerical":
+                bundle = bundle
+            else:
+                raise ValueError("Config modality not specified")
+        else:
+            raise ValueError("Config dynamics not specified")
 
-    # Save dataset
-    folder_name = "../saved_datasets/" + time.strftime("%Y%m%d") + "-"
-    idx = 0
-    while os.path.exists(folder_name + str(idx) + "/"):
-        idx += 1
-    folder_name = folder_name + str(idx)
-    os.mkdir(folder_name)
-    for key in bundle.keys():
-        np.save(folder_name + "/" + key, bundle[key])
-    shutil.copy(config_file, folder_name + "/config.json")
+        # Save dataset
+        folder_name = saved_dir + "/" + time.strftime("%Y%m%d") + "-"
+        idx = 0
+        while os.path.exists(folder_name + str(idx) + "/"):
+            idx += 1
+        folder_name = folder_name + str(idx)
+        os.mkdir(folder_name)
+        for key in bundle.keys():
+            np.save(folder_name + "/" + key, bundle[key])
+
+        with open(folder_name + "/config.json", "w") as f:
+            json.dump(config, f)
+
+    dataset = ConservationDataset(bundle)
+
+    return dataset, folder_name
+
+#get_dataset("orbit_config_default.json")
+
 
 # Template to test distribution generation.
 """class TestDistribution:
     def __init__(self):
-        self.type = "exponential"
-        self.dims = 1
-        self.scale = 1
-        self.shift = -1
+        self.type = "uniform"
+        self.dims = 2
+        self.min = [0,1]
+        self.max = [1,5]
 
 dist = TestDistribution()
+sample = sample_distribution(dist, 1000)
 
-plt.scatter(np.zeros((1000,)), sample_distribution(dist, 1000), s=0.1)
+plt.scatter(sample[:, 0], sample[:, 1], s=0.1)
 plt.show()"""
+
+#np.save("temp_orbit_check", orbits_num_gen(10000)[0])
