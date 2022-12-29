bb6fb49ce0e06cbb1a3c14ec9f3ff2874e8bd8be
Ali Cy <califynic@gmail.com>
2022-12-28 19:01:31 -0500

    2d plots now work, more documentation for vis

--- ldcl/plot/color.py
+++ ldcl/plot/color.py
@@ -22,8 +22,12 @@ def get_cmap(cmap, normalize=True):
 
     def normalize_wrap(x):
         x = x - np.min(x)
-        x = x / np.max(x)
-        return x
+
+        if np.max(x) < 1e-12: # the values are probably all zero
+            return np.full(np.shape(x), 0.5) 
+        else:
+            x = x / np.max(x)
+            return x
 
     if normalize:
         return lambda x: func(normalize_wrap(x))
--- run/visualizations/single_orbit_plot.py
+++ run/visualizations/single_orbit_plot.py
@@ -12,33 +12,54 @@ oneD_span, _ = get_dataset("../data_configs/H_vary.json", "../../saved_datasets"
 single_orbit_embeds, single_orbit_vals = embed("../saved_models/rmse_vanilla/final_encoder.pt", single_orbit)
 oneD_span_embeds, oneD_span_vals = embed("../saved_models/rmse_vanilla/final_encoder.pt", oneD_span)
 
+"""
+# Dim reduction (2d only).
+pca = PCA(n_components=2) # dimensionality reduction for 2D
+single_orbit_embeds = pca.fit_transform(single_orbit_embeds)
+oneD_span_embeds = pca.transform(oneD_span_embeds)
+"""
+
 # Colors
 
 viridis = get_cmap('viridis')
 plasma = get_cmap('plasma')
-#ylorrd = get_cmap('YlOrRd')
-#pubugn = get_cmap('PuBuGn')
 blank = get_cmap('blank')
-
-# Creating size/colors
-
-single_orbit_colors = viridis(single_orbit_vals["x"])
-oneD_span_colors = plasma(oneD_span_vals["x"])
-
-single_orbit_sizes = 4
-oneD_span_sizes = 2
 
 # Plot
 
-plot = VisPlot(3) # 3D plot, 2 for 2D plot
-plot.add(single_orbit_embeds, 
-    size=single_orbit_sizes, 
-    color=single_orbit_colors, 
-    labels=single_orbit_vals, 
-    outline=False)
-plot.add(oneD_span_embeds, 
-    size=oneD_span_sizes, 
-    color=oneD_span_colors, 
-    labels=oneD_span_vals, outline=False)
+# option 1: add syntax (more flexible, more work)
+def add_demo():
+    plot = VisPlot(3) # 3D plot, 2 for 2D plot
+
+    single_orbit_colors = viridis(single_orbit_vals["x"])
+    oneD_span_colors = plasma(oneD_span_vals["x"])
+
+    # Note that a list of sizes can be passed in too.
+
+    plot.add(oneD_span_embeds, 
+        size=2, 
+        color=oneD_span_colors, 
+        label=oneD_span_vals, outline=False)
+    plot.add(single_orbit_embeds, 
+        size=4, 
+        color=single_orbit_colors, 
+        label=single_orbit_vals, outline=False)
+
+    return plot
+
+# option 2: add_with_cmap syntax (fastest, less flexible)
+# note that you can put a list of cmap/cby to make multiple plots
+# this one has both x and H, not just x
+
+def add_with_cmap_demo():
+    plot = VisPlot(3, num_subplots=2) # 3D plot, 2 for 2D plot
+
+    plot.add_with_cmap(oneD_span_embeds, oneD_span_vals, cmap="viridis", cby=["x", "H"], size=0.5, outline=False)
+    plot.add_with_cmap(single_orbit_embeds, single_orbit_vals, cmap="plasma", cby=["x", "H"], size=4, outline=False)
+
+    return plot
+
+#plot = add_demo()
+plot = add_with_cmap_demo()
 
 plot.show()
